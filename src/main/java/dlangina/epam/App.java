ed00ee4883014d409874d2b9451db946d123ab11
DariaLangina <dlangina@rvision.ru>
2022-02-26 16:48:01 +0300

    added pom.xml and examples

---
+++ src/main/java/dlangina/epam/App.java
@@ -0,0 +1,13 @@
+package dlangina.epam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
